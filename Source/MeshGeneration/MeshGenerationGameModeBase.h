f397b9cd9f9a1d66aea91442412db3e6c7d570de
Shiqi Lian <shiqi.lian.stuttgart@gmail.com>
2019-12-18 20:02:32 +0100

    init for meshGenerator

---
+++ Source/MeshGeneration/MeshGenerationGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MeshGenerationGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MESHGENERATION_API AMeshGenerationGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
